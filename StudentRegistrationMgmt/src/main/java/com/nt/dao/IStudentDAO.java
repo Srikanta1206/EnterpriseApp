257bf2fdc4682481b8074d72a839461fb7f2d197
Lenovo <Lenovo@DESKTOP-2P52UUT>
2023-06-14 18:30:28 +0530

    done

--- StudentRegistrationMgmt/src/main/java/com/nt/dao/IStudentDAO.java
+++
@@ -1,9 +0,0 @@
-package com.nt.dao;
-
-import com.nt.bo.StudentBO;
-
-public interface IStudentDAO {
-	int insertRecord(StudentBO bo) throws Exception;
-	boolean retriveRecord(StudentBO bo) throws Exception;
-	
-}
--- StudentRegistrationMgmt/src/main/java/com/nt/service/IStudentMgmtService.java
+++
@@ -1,8 +0,0 @@
-package com.nt.service;
-
-import com.nt.dto.StudentDTO;
-
-public interface IStudentMgmtService {
-	public String registerStudent(StudentDTO dto) throws Exception;
-	public String studentResult(StudentDTO dto) throws Exception;
-}
